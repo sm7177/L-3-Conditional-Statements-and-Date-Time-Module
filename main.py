c173add61f20d9e11157730fa4a81ee238109621
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-23 16:54:05 +0545

    activity

---
+++ main.py
@@ -0,0 +1,77 @@
+# Activity 1
+# number=int(input("Enter a number"))
+
+# if number %2==0:
+#     print("number is even",number)
+    
+# else:
+#     print("number is odd",number)
+    
+    
+# name=input("enter your name")
+# print("Your name is",name)
+
+
+
+
+# Activity 2
+
+# height=float(input("Enter yout height in cm:"))
+# weight=float(input("Enter your weight in kg:"))
+# name=input("Enter your name")
+
+# BMI= weight/(height/100)**2
+
+# print("your BMI is:",BMI)
+
+# if BMI <=18.4:
+#     print(name,"is underweight")
+# elif BMI <=24.9:
+#     print(name,"is healthy")
+# elif BMI <=29.9:
+#     print(name,"is overweight")
+# elif BMI <=34.9:
+#     print(name,"is severely overweight")
+# else:
+#     print(name,"you are obese.")
+    
+    
+    
+    
+    
+# Activity 3
+# num=int(input("enter a number to check:"))
+
+# if num>50:
+#     print("the number is greater than 50")
+#     if num%2==0:
+#         print("the number is even")
+#     else:
+#         print("the number is odd")
+# else:
+#     print("the number is less than or equal to 50")
+
+
+
+
+#Activity 4
+
+# import datetime
+
+# current_time = datetime.datetime.now()
+
+# print("Current time: ", current_time)
+
+# import calendar
+
+# x=calendar.calendar(2025)
+# print(x)
+
+
+
+num=int(input("Enter a number to check:"))
+
+if num>0:
+    print("the number is positive")
+else:
+    print("the number is negative")
